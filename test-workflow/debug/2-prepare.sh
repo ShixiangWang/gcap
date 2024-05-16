958a13539e012c4974dd00c90e667389c71fc23a
Shixiang Wang (王诗翔) <wang.shixiang@icloud.com>
2024-05-16 12:57:52 +0800

    update debug workflow

--- test-workflow/debug/2-prepare.sh
+++ test-workflow/debug/2-prepare.sh
@@ -1,5 +1,5 @@
 # Set up conda env
-# mamba create -n cancerit -c bioconda cancerit-allelecount 
+# mamba create -n cancerit -c bioconda cancerit-allelecount
 
 cd /data3/wsx/share/gcap_reference
 wget -c https://zenodo.org/records/6524005/files/1000G_loci_hg38.tar.gz
@@ -9,3 +9,9 @@ wget -c https://zenodo.org/records/6524005/files/RT_correction_hg38.txt.gz
 tar zxvf 1000G_loci_hg38.tar.gz
 gunzip GC_correction_hg38.txt.gz
 gunzip RT_correction_hg38.txt.gz
+
+# For the latest version of ASCAT, please use the following correction files instead
+# See https://github.com/VanLoo-lab/ascat/tree/master/ReferenceFiles/WES for all WES reference files
+#
+# wget -c https://zenodo.org/records/10513234/files/GC_G1000_WES_hg38.zip
+# wget -c https://zenodo.org/records/10513234/files/RT_G1000_WES_hg38.zip
--- test-workflow/debug/3-gcap.R
+++ test-workflow/debug/3-gcap.R
@@ -7,18 +7,20 @@ library(gcap)
 # id为PRJEB42904，wes_395LC是tumor，id：ERR5242993，wes_395N是normal，id：ERR5243012
 
 # hg38 ----------------
+
+# note: Please update the reference files if you use different version of ASCAT
 gcap.workflow(
-  tumourseqfile = "~/share/gcap_debug/bam/ERR5242993.bam", 
+  tumourseqfile = "~/share/gcap_debug/bam/ERR5242993.bam",
   normalseqfile = "~/share/gcap_debug/bam/ERR5243012.bam",
   tumourname = "wes_395LC",
   normalname = "wes_395N",
   jobname = "wes_395",
   outdir = "~/share/gcap_debug/gcap_result",
-  allelecounter_exe = "~/miniconda3/envs/cancerit/bin/alleleCounter", 
+  allelecounter_exe = "~/miniconda3/envs/cancerit/bin/alleleCounter",
   g1000allelesprefix = file.path(
     "~/share/gcap_reference/1000G_loci_hg38/",
     "1kg.phase3.v5a_GRCh38nounref_allele_index_chr"
-  ), 
+  ),
   g1000lociprefix = file.path("~/share/gcap_reference/1000G_loci_hg38/",
                               "1kg.phase3.v5a_GRCh38nounref_loci_chrstring_chr"
   ),
